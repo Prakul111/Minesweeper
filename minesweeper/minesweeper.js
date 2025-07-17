8cde1cc75b8c9a4b3049f41fae72d6e171b02915
@Prakul111 <iamprakul111@gmail.com>
2025-07-17 21:48:26 +0530

    reveal tile

--- minesweeper/minesweeper.js
+++ minesweeper/minesweeper.js
@@ -1,6 +1,6 @@
 //logic
 
-const TILE_STATUSES = {
+export const TILE_STATUSES = {
     HIDDEN: 'hidden',
     MINE: 'mine',
     NUMBER: 'number',
@@ -8,11 +8,12 @@ const TILE_STATUSES = {
 }
 
 
-
-
-
 export function createboard(boardSize, numberOfMines) {
     const board = []
+    const minePostions = getMinePositions(boardSize, numberOfMines)
+    console.log(minePostions);
+
+
     for (let x = 0; x < boardSize; x++) {
         const row = []
         for (let y = 0; y < boardSize; y++) {
@@ -21,7 +22,14 @@ export function createboard(boardSize, numberOfMines) {
             const tile = {
                 element,
                 x,
-                y
+                y,
+                mine: minePostions.some(positionMatch.bind(null, { x, y })),
+                get status() {
+                    return this.element.dataset.status
+                },
+                set status(value) {
+                    this.element.dataset.status = value
+                }
             }
             row.push(tile)
         }
@@ -29,3 +37,87 @@ export function createboard(boardSize, numberOfMines) {
     }
     return board
 }
+
+function getMinePositions(boardSize, numberOfMines) {
+    const positions = []
+
+    while (positions.length < numberOfMines) {
+        const position = {
+            x: randomNumber(boardSize),
+            y: randomNumber(boardSize),
+        }
+
+        if (!positions.some(positionMatch.bind(null, position))) {
+            positions.push(position)
+        }
+    }
+
+    return positions
+}
+
+
+export function markTile(tile) {
+    if (tile.status !== TILE_STATUSES.HIDDEN
+        && tile.status !== TILE_STATUSES.MARKED) {
+        return
+    }
+
+
+    if (tile.status === TILE_STATUSES.MARKED) {
+        tile.status = TILE_STATUSES.HIDDEN
+    } else {
+        tile.status = TILE_STATUSES.MARKED
+    }
+}
+
+
+export function revealTile(board, tile) {
+    if (tile.status !== TILE_STATUSES.HIDDEN) {
+        return
+    }
+    if (tile.mine) {
+        tile.status = TILE_STATUSES.MINE
+        return
+    }
+
+    tile.status = TILE_STATUSES.NUMBER
+    const adjacentTiles = neabyTiles(board, tile)
+    const mines = adjacentTiles.filter(t => t.mine)
+    if (mines.length === 0) {
+        adjacentTiles.forEach(revealTile.bind(null, board))
+    } else {
+        tile.element.textContent = mines.length
+    }
+}
+
+
+function positionMatch(a, b) {
+    return a.x === b.x && a.y === b.y
+}
+
+function randomNumber(size) {
+    return Math.floor(Math.random() * size)
+}
+
+
+function neabyTiles(board, { x, y }) {
+    const tiles = []
+
+    for (let xOffset = -1; xOffset <= 1; xOffset++) {
+        for (let yOffset = -1; yOffset <= 1; yOffset++) {
+            const tile = board[x + xOffset]?.[y + yOffset]
+            if (tile) tiles.push(tile)
+        }
+    }
+
+    return tiles
+}
+
+
+
+
+
+
+
+
+
--- minesweeper/script.js
+++ minesweeper/script.js
@@ -1,42 +1,50 @@
 //display/ui
-import { createboard } from "./minesweeper.js";
+import { createboard, markTile, TILE_STATUSES, revealTile } from "./minesweeper.js";
 
 const BOARD_SIZE = 10
-const NUMBER_OF_MINES = 2
+const NUMBER_OF_MINES = 10
 
 
 const board = (createboard(BOARD_SIZE, NUMBER_OF_MINES));
 const boardElement = document.querySelector('.board')
+const mineLeftText = document.querySelector('[data-mine-count]')
 
 
 
 board.forEach(row => {
     row.forEach(tile => {
         boardElement.append(tile.element)
+        tile.element.addEventListener('click', () => {
+            revealTile(board, tile)
+        })
+        tile.element.addEventListener('contextmenu', e => {
+            e.preventDefault()
+            markTile(tile)
+            listMinesLeft()
+        })
     })
 })
 
 
 
 boardElement.style.setProperty('--size', BOARD_SIZE)
+mineLeftText.textContent = NUMBER_OF_MINES
 
 
 
-//Populate a board with tiles/mines
+function listMinesLeft() {
+    const markedTilesCount = board.reduce((count, row) => {
+        return count + row.filter(tile => tile.status === TILE_STATUSES.MARKED).length
+    }, 0)
+
+    mineLeftText.textContent = NUMBER_OF_MINES - markedTilesCount
+}
 
 
 
-// left click on tiles
 
 
-//reveal tiles
 
 
-//right click on tiles
 
-
-//mark tiles
-
-
-//check for win/lose
 
